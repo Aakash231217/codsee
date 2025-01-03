c8f5bfec07616308d1695ef1f44c58453025f6de
aakash <rktaakash@gmail.com>
2025-01-03 23:46:51 +0530

    frontend

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -19,6 +19,10 @@ const Sidebarlayout = ({children}:Props) => {
             </div>
             <UserButton />
         </div>
+        <div className='h-4'></div>
+        <div className='border-sidebar-border bg-sidebar border shadow rounded-md overflow-y-scroll h-[calc(100vh-6rem)] p-4'>
+          {children}
+        </div>
       </main>
     </SidebarProvider>
   )
